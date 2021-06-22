f0ce622fbffbfcd04ce25357ef2106d24672bc93
mintuta <mintefikr4@gmail.com>
2021-06-22 17:07:08 -0500

    Q1 added

--- Assignment two/Q1/app.js
+++ Assignment two/Q1/app.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.set("port", 5000);
+app.set("port", 5050);
 
 
 app.get("/", function (req, resp) {
     resp.status(200).sendFile(path.join(__dirname, "public", "index.html"))
-    //console.log(__dirname)
-})
+ })
 
 
-app.listen(app.get("port"), function () {
-    console.log("start port number", app.get("port"))
+const server=app.listen(app.get("port"), function () {
+    const ports=server.address.port;
+    console.log("start server with port number", app.get("port"));
 })
 
 
