3f26dbff4827add9e2389d2c7d7bab06835abc91
mintuta <mintefikr4@gmail.com>
2021-07-14 23:03:01 -0500

    package update

--- Second week/Game Project/api/routes/index.js
+++ Second week/Game Project/api/routes/index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const controllerGames = require("../controllers/games.Controller");
 const controllerPublisher = require("../controllers/publisher.Controller");
@@ -6,32 +5,31 @@ const controllerUsers = require("../controllers/users.Controller");
 
 const router = express.Router();
 
-router.route("/games")
-    .get(controllerGames.gamesGetAll)
-    .post(controllerUsers.authenticate, controllerGames.gamesAddOne);
+router
+  .route("/games")
+  .get(controllerGames.gamesGetAll)
+  .post(controllerUsers.authenticate, controllerGames.gamesAddOne);
 
-router.route("/games/:gameID")
-    .get(controllerGames.gamesGetOne)
-    .put(controllerUsers.authenticate, controllerGames.gamesFullUpdateOne)
-    .patch(controllerUsers.authenticate, controllerGames.gamesPartialUpdateOne)
-    .delete(controllerUsers.authenticate, controllerGames.gamesDeleteOne)
+router
+  .route("/games/:gameID")
+  .get(controllerGames.gamesGetOne)
+  .put(controllerUsers.authenticate, controllerGames.gamesFullUpdateOne)
+  .patch(controllerUsers.authenticate, controllerGames.gamesPartialUpdateOne)
+  .delete(controllerUsers.authenticate, controllerGames.gamesDeleteOne);
 
-router.route("/games/:gameID/publishers")
-    .post(controllerUsers.authenticate, controllerPublisher.publisherAddOne)
-    .get(controllerPublisher.publisherGetAll);
+router
+  .route("/games/:gameID/publishers")
+  .post(controllerUsers.authenticate, controllerPublisher.publisherAddOne)
+  .get(controllerPublisher.publisherGetAll);
 
-router.route("/games/:gameID/publishers/:publisherId")
-    .get(controllerPublisher.publisherGetOne)
-    .put(controllerUsers.authenticate, controllerPublisher.publisherFUllUpdateOne)
-    .delete(controllerUsers.authenticate, controllerPublisher.publisherDeleteOne);
+router
+  .route("/games/:gameID/publishers/:publisherId")
+  .get(controllerPublisher.publisherGetOne)
+  .put(controllerUsers.authenticate, controllerPublisher.publisherFUllUpdateOne)
+  .delete(controllerUsers.authenticate, controllerPublisher.publisherDeleteOne);
 
-router.route("/users")
-    .post(controllerUsers.register);
+router.route("/users").post(controllerUsers.register);
 
-router.route("/users/login")
-    .post(controllerUsers.login);
-
-
-
+router.route("/users/login").post(controllerUsers.login);
 
 module.exports = router;
--- Second week/Game Project/app.js
+++ Second week/Game Project/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 const db = require("./api/data/db");
 // const bodyParser =require("body-parser");
 const app = express();
@@ -7,11 +8,22 @@ const gameroutes = require("./api/routes/index");
 
 app.set("port", 3000);
 
+const corsOptions = {
+  origin: "http://localhost:4200/",
+  optionsSuccessStatus: 200,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors());
 app.use(function (req, resp, next) {
   console.log(req.method, req.url);
   next();
 });
-
+app.get("/api", function (req, res, next) {
+  res.json({ msg: "This Cors enable for http://localhost:4200" });
+  console.log("enabled");
+});
 app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
